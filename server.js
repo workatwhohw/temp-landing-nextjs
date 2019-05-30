fe9f11df1e7036b7fc23ee6e012db89d2b7cbd24
Andrew Defee <me@andrewdefee.com>
2019-05-30 04:15:41 -0400

    deploy

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app
   .prepare()
   .then(() => {
     const server = express();
-    server.set("port", parseInt(process.env.PORT, 10) || 8081);
+    server.set("port", process.env.PORT || 8081);
     server.use(nextI18NextMiddleware(nextI18next));
 
     // Dev environment whitelist. Add your local env to avoid https redir
-    const localDev = ["localhost:8081", "whohw.us-east-2.elasticbeanstalk.com"];
+    const localDev = ["localhost:8081", "whohw-stage.us-east-2.elasticbeanstalk.com"];
 
     // Force HTTPS if we're on live. Whitelist dev envs above to avoid redirection.
     // Note: Technically doing string comparison here, which has some potential security concerns, but don't have many options because of how AWS load balancer works
