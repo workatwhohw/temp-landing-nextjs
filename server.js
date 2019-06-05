00101da9d2a5476149fbc55949ec29be5ac4fcf1
Andrew Defee <me@andrewdefee.com>
2019-06-04 20:30:24 -0400

    Cleaning up packaging

--- server.js
+++ server.js
@@ -2,42 +2,42 @@
  * Server Logic
  */
 
-const express = require("express");
-const next = require("next");
-const nextI18NextMiddleware = require("next-i18next/middleware");
-const nextI18next = require("./i18n");
-const { parse } = require("url");
-const mobxReact = require("mobx-react");
+require("dotenv").config()
+const express = require("express")
+const next = require("next")
+const nextI18NextMiddleware = require("next-i18next/middleware")
+const nextI18next = require("./i18n")
+const { parse } = require("url")
+const mobxReact = require("mobx-react")
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== "production"
+const disableSSLCheck = process.env.DISABLE_SSL == true
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-mobxReact.useStaticRendering(true);
+mobxReact.useStaticRendering(true)
 
 app
   .prepare()
   .then(() => {
-    const server = express();
-    server.set("port", process.env.PORT || 8081);
-    server.use(nextI18NextMiddleware(nextI18next));
+    const server = express()
+    server.set("port", process.env.PORT || 8081)
+    server.use(nextI18NextMiddleware(nextI18next))
 
-    // Dev environment whitelist. Add your local env to avoid https redir
-    const localDev = ["localhost:8081", "whohw-stage.us-east-2.elasticbeanstalk.com"];
+    // Dev environment whitelist. Add envs to avoid https redir
+    const localDev = ["localhost:8081", "dev.workatwhohw.com"]
 
     // Force HTTPS if we're on live. Whitelist dev envs above to avoid redirection.
     // Note: Technically doing string comparison here, which has some potential security concerns, but don't have many options because of how AWS load balancer works
     function forceSSL(req, res, next) {
-      if (
-        req.headers["x-forwarded-proto"] === "https" ||
-        localDev.includes(req.headers.host)
-      ) {
-        return next();
+
+      if (req.headers["x-forwarded-proto"] === "https" || localDev.includes(req.headers.host)) {
+        return next()
       }
-      res.redirect("https://" + req.headers.host);
+      res.redirect('https://' + req.headers.host + req.url)
     }
 
-    server.all("*", forceSSL);
+    server.all("*", forceSSL)
 
     /**
      * Routes
@@ -53,19 +53,19 @@ app
 
     // Handle all other requests as local /pages (home, about, etc)
     server.get("*", (req, res) => {
-      const parsedUrl = parse(req.url, true);
-      handle(req, res, parsedUrl);
+      const parsedUrl = parse(req.url, true)
+      handle(req, res, parsedUrl)
     });
 
     /**
      * Init
      */
     server.listen(server.get("port"), err => {
-      if (err) throw err;
-      console.log("Server listening on port " + server.get("port"));
+      if (err) throw err
+      console.log("Server listening on port " + server.get("port"))
     });
   })
   .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
+    console.error(ex.stack)
+    process.exit(1)
   });
