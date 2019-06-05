540c6f06e5e769f1dd0a563c603596040747324c
Andrew Defee <me@andrewdefee.com>
2019-06-04 22:24:41 -0400

    Added Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,10 @@ export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     // Check if in production
     const isProduction = process.env.NODE_ENV === 'production';
+    const analyticsID = (process.env.GOOGLE_ANALYTICS ? process.env.GOOGLE_ANALYTICS : ''); // Not currently used
     const initialProps = await Document.getInitialProps(ctx);
     // Pass isProduction flag back through props
-    return { ...initialProps, isProduction };
+    return { ...initialProps, isProduction, analyticsID };
   }
 
   setGoogleTags() {
@@ -35,7 +36,7 @@ export default class MyDocument extends Document {
             <Fragment>
               <script
                 async
-                src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXX-X"
+                src="https://www.googletagmanager.com/gtag/js?id=UA-141540125-1"
               />
               {/* We call the function above to inject the contents of the script tag */}
               <script dangerouslySetInnerHTML={this.setGoogleTags()} />
